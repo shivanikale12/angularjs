b6555853b87e006598f50f2b81d1f53b80e67099
shivanikale12 <63407084+shivanikale12@users.noreply.github.com>
2020-04-17 13:52:10 +0530

    Add files via upload

---
+++ Scripts/app.js
@@ -0,0 +1,27 @@
+console.log("Hi");
+  var app = angular.module('sample',["ngRoute"]);
+app.config(function($routeProvider){
+    console.log("Hi2");
+    $routeProvider.
+    when("/NewEvent",{
+        templateUrl : "add_event.html",
+        controller: "AddEventController"
+    }).
+    when("/DisplayEvent", {
+        templateUrl: "/show_event.html",
+        controller: "ShowDisplayController"
+    }).
+    otherwise ({
+        redirectTo: '/DisplayEvent'
+    });
+});
+app.controller("AddEventController", function($scope) {
+    console.log("AddEventController");
+    $scope.message = "This is to Add a new Event";
+
+});
+app.controller("ShowDisplayController",function($scope){
+    console.log("ShowDisplayController");
+    $scope.message = "This is display an Event";
+
+});
